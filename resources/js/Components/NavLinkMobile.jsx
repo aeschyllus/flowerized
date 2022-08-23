15ff7eaddcae02bb6152f0fb7baf4d00e522c0b9
aeschyllus <lapadamac@gmail.com>
2022-08-23 19:17:10 +0800

    Changed mobile links to uppercase

--- resources/js/Components/NavLinkMobile.jsx
+++ resources/js/Components/NavLinkMobile.jsx
@@ -3,7 +3,7 @@ import { Link } from "@inertiajs/inertia-react";
 export default function NavLinkMobile({ href, active, name }) {
   return (
     <li>
-      <Link href={href}>
+      <Link href={href} className="uppercase">
         {active && (
           <span className="absolute mt-2 left-3 bg-red-500 w-2 h-2 rounded-full"></span>
         )}
