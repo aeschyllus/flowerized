972285fc20746895b8bcfc11100efe21d4a455c4
aeschyllus <lapadamac@gmail.com>
2022-08-25 08:54:41 +0800

    Modified app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,8 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        /* if(env('APP_ENV') !== 'local') { */
-        /*     $url->forceScheme('https'); */
-        /* } */
+        if(App::environment(['staging', 'production'])) {
+            $url->forceScheme('https');
+        }
     }
 }
